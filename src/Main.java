7b9dff0829e9a51cd06b1f0dfe3e410bcb02ef6e
1 <Anasko162@Mail.ru>
2022-11-29 18:30:02 +0300

    Дз по теме Строки

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+        public static void main(String[] args) {
+            String firstName = "Иван";
+            String middleName = "Иванович";
+            String lastName = "Иванов";
+            String separator = " ";
+            String fullName = String.join(separator, lastName, firstName, middleName);
+            System.out.println("ФИО сотрудника — " +fullName+ ".");
+            String fullName1 = "Ivanov Ivan Ivanovich";
+            System.out.println("Данные ФИО сотрудника для заполнения отчета — "+fullName1.toUpperCase());
+            fullName1 = "Иванов Семён Семёнович".replace("ё","е");
+            System.out.println(fullName1);
+
+        }
     }
-    int 10
-}
